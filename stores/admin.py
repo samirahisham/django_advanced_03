412093c88ce6c33258aede5f80b6cc93fbf858e9
samirahisham <engineersamiraawad@gmail.com>
2019-09-01 11:59:29 +0300

    sara and samira

--- stores/admin.py
+++ stores/admin.py
@@ -2,4 +2,19 @@ from django.contrib import admin
 from .models import Store
 
 # Register your models here.
-admin.site.register(Store)
+
+
+
+class StoreAdmin(admin.ModelAdmin):
+    
+    search_fields = ['description','name']
+    list_filter = ['added']
+    list_display = ('id','name', 'description')
+    list_display_links=('name',)
+  
+    list_editable=('description',)
+
+
+
+
+admin.site.register(Store,StoreAdmin)
